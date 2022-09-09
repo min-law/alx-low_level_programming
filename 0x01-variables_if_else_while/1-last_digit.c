d8fdbf321a7c89616a75ba6b7afa2869fc2a5593
lawrence Akinboye <lawrenceakinboye@gmail.com>
2022-09-09 07:52:48 -0700

    adlo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,12 +22,10 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-
 	else if (l == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
-
 	else 
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
