6b5b7e7b63ed6fa5859e48021b5c9c24602dccfa
lawrence Akinboye <lawrenceakinboye@gmail.com>
2022-09-09 08:11:16 -0700

    agdd

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,14 @@
 int main(void)
 
 {
-	char lower_case;
-	char upper_case;
-
-	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-		putchar(lower_case);
+		putchar(ch);
 	}
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
 	{
-		putchar(upper_case);
+		putchar(ch);
 	}
 
 	putchar('\n');
