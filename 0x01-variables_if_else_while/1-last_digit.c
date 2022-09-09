a5529a3ebe9e9db52395e5c77df1e0e1a7a5fdab
lawrence Akinboye <lawrenceakinboye@gmail.com>
2022-09-09 08:00:03 -0700

    agddo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,8 @@ int main(void)
 	else if (l == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, l);
-	}else 
+	}
+	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
