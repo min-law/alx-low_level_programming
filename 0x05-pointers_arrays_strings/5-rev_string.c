0bd42dc63dd8dc89e94471676affac7bf3d7102b
lawrence Akinboye <51114125+min-law@users.noreply.github.com>
2022-09-19 18:01:55 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,31 @@
-ooo
+#include "main.h"
+
+/**
+* rev_strings - reverse the string.
+* @s : pointer to string
+*
+* Return : void.
+*/
+
+void rev_string(char *s)
+{
+	int i, j, k, temp;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	K = 0;
+	J = I - 1;
+
+	while (K < J)
+	{
+		temp = s[k];
+		s[k] = s[j];
+		s[j] = temp;
+		k++;
+		j--;
+	}
+}
