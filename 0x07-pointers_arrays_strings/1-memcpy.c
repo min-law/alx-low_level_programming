dc5e218f90de6f617410cea23c7f7fd3b54b790c
lawrence Akinboye <51114125+min-law@users.noreply.github.com>
2022-09-26 21:14:42 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,8 +8,7 @@
 * 
 * Return: pointer to the copied memory block
 */
-
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
