adf18bcb6d6529a4868f1e9792b8bb5c2350df17
lawrence Akinboye <51114125+min-law@users.noreply.github.com>
2022-09-27 19:05:32 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,32 @@
 #include "main.h"
 
 /**
-* _strstr - locates a substring
-* @haystack: main str to be examined
-* @needle: searched in haystack
-*
-* Return: 0
-*/
+ * _strstr - locates a substring
+ * @haystack: string in which to check for needle
+ * @needle: substring to find in haystack
+ *
+ * Return: pointer to beginning of needle in haystack or NULL if no match
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	char *str1, *str2;
+	unsigned int i = 0, j = 0;
 
-	while (*haystack != '\0')
+	while (haystack[i])
 	{
-		str1 = haystack;
-		str2 = needle;
-
-		/* Star WHILE*/
-		while (*haystack != '\0' && *str2 != '\0' && haystack == *str2)
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			haystack++;
-			str2++;
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
+				break;
 		}
-		if (*str2 == '\0')
-			return (str1);
-		haystack = str1 + 1;
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
 	}
 	return (0);
 }
