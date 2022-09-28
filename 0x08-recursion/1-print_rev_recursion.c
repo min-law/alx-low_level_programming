6725297992245121a3505ef5f0df32fada9106ca
lawrence Akinboye <51114125+min-law@users.noreply.github.com>
2022-09-28 11:08:00 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,16 @@
-11
+#include "main.h"
+
+/**
+ * _puts_recursion - print's a string in reverse
+ * @s: string to be printed
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
