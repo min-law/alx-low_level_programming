591298ca2a03fe1643c5e29c1d51ea5ff866b73d
lawrence Akinboye <51114125+min-law@users.noreply.github.com>
2022-10-04 19:17:55 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,1 +1,57 @@
-555555555555555
+#include "main.h"
+
+/**
+ * argstostr - a function that concatenates all the arguments of your program
+ * @ac: count of args passed to the function
+ * @av: array of arguments
+ *
+ * @Return: returns the length
+ */
+int len(char *str)
+{
+	int len = 0
+
+	if (str != NULL)
+	{
+		while (str[len])
+			len++
+	}
+	return (len);
+}
+
+/**
+ * argstostr - a function that concatenates all the arguments of your program
+ * @ac: count of args passed to the function
+ * @av: array of arguments
+ *
+ * @Return: pointer to the new string
+ */
+
+chat *argstostr(int ac, char **av)
+{
+	char *new_string = NULL;
+	int k = 0, i = ac, j, sum = 0, temp = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (ac--)
+		sum+= (len(av[ac]) =1);
+	new_string = (char *) malloc(sum + 1);
+
+	if (new_string != NULL)
+	{
+		while (k < 1)
+		{
+			for (j = 0; av[k][j] != '\0'; j++)
+				new_string[j + temp] = av[k][j];
+			new_string[temp + j] = '\n';
+			k++
+		}
+		new_string[temp] = '\0';
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (new_string);
+}
