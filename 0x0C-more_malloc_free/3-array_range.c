532900c0f4f83c6641bb894fec88e377849c5d03
lawrence Akinboye <51114125+min-law@users.noreply.github.com>
2022-10-07 09:58:16 +0100

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	a = malloc(sizeof(int) * l);
 	if ( a== NULL)
 		return (NULL);
-	for (i = 0, i < l; i++, min++)
+	for (i = 0; i < l; i++, min++)
 	{
 		a[i] = min;
 	}
