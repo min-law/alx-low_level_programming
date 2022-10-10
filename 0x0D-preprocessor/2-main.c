40c032f51de32beee875414b50a3f0dac55a7843
lawrence Akinboye <51114125+min-law@users.noreply.github.com>
2022-10-10 18:54:21 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,12 @@
-2
+#include <stdio.h>
+/**
+ * main - prints the name of the file program was compiled from
+ *
+ * @Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
